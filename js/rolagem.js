095422a1c42c13dd8f17005b8c15ae1dafb01c22
VictorLessa <victordsgnr@gmail.com>
2018-03-25 12:38:16 -0300

    s

---
+++ js/rolagem.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+var firtscroll = 0;
+$(document).ready(function(){
+	$(window).scroll(function(){
+		
+			var top = $(this).scrollTop();
+					if(firtscroll > top){
+						console.log(top);
+					}else{
+						console.log(top, 'descendo');
+					}
+		firtscroll = top;
+});
+});
