f4498b53b2d2b71d8277459069317f3239352b0b
VictorLessa <victordsgnr@gmail.com>
2018-04-24 21:20:01 -0300

    s

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+
+	function togglemenu(){
+		$('.bar-top').toggleClass('bar-top-active');
+		$('.bar-bottom').toggleClass('bar-bottom-active');
+		$('.main').toggleClass('main-active');
+		$('.link-item').toggleClass('link-item-active');
+
+	}
+
+	$('.barr').on('click tap', function(){
+	    togglemenu();
+	  });
+
+	 $(document).keyup(function(e) {
+	    if (e.keyCode === 27) {
+	      togglemenu();
+	    }
+	  });
+	
+});
